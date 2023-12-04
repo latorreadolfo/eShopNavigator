85408d6352b132478c37a651f0824ee780f0b58f
latorreadolfo <adolfo3eng@gmail.com>
2023-12-04 13:06:13 -0300

    eshop navigation app done

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,106 @@
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React, {PropsWithChildren} from 'react';
+
+type ProductProps = PropsWithChildren<{
+  product: Product;
+}>;
+
+const ProductItem = ({product}: ProductProps) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: product.imageUrl}} style={styles.image} />
+      <View>
+        <Text style={styles.name}>{product.name}</Text>
+
+        <View style={[styles.rowContainer, styles.ratingContainer]}>
+          <View style={styles.rating}>
+            <Text style={styles.ratingText}>{product.rating} ★</Text>
+          </View>
+          <Text style={styles.ratingCount}>
+            ({product.ratingCount.toLocaleString()})
+          </Text>
+        </View>
+        <View style={[styles.rowContainer, styles.priceContainer]}>
+          <Text style={styles.originalPrice}>
+            G${product.originalPrice.toLocaleString()}
+          </Text>
+          <Text style={styles.discountPrice}>
+            G${product.discountPrice.toLocaleString()}
+          </Text>
+          <Text style={styles.offerPercentage}>
+            %{product.offerPercentage} off
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 8,
+    flexDirection: 'row',
+  },
+  rowContainer: {
+    flexDirection: 'column',
+  },
+  image: {
+    width: 90,
+    height: 150,
+    resizeMode: 'contain',
+
+    marginRight: 12,
+  },
+  name: {
+    marginBottom: 4,
+
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  ratingContainer: {
+    marginBottom: 8,
+    maxWidth: 51,
+  },
+  priceContainer: {
+    marginBottom: 12,
+  },
+  rating: {
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    justifyContent: 'center',
+    backgroundColor: '#008c00',
+
+    marginRight: 4,
+  },
+  ratingText: {
+    color: '#fff',
+    fontSize: 12,
+    fontWeight: '600',
+  },
+  ratingCount: {
+    color: '#878787',
+    fontSize: 11,
+  },
+  originalPrice: {
+    fontSize: 14,
+    marginRight: 6,
+    fontWeight: '600',
+
+    color: 'rgba(0, 0, 0, 0.5)',
+    textDecorationLine: 'line-through',
+  },
+  discountPrice: {
+    fontSize: 16,
+    marginRight: 6,
+    fontWeight: '600',
+
+    color: '#000000',
+  },
+  offerPercentage: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: '#4bb550',
+  },
+});
+
+export default ProductItem;
